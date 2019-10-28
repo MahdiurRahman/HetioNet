32ed1ed2ecfcfca4cb2d527d482338e78a47c71e
Mahdiur Rahman <rmahdiur@gmail.com>
2019-10-28 01:57:27 -0400

    can read nodes.tsv file

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
+print('\n')
 import sys
-print(sys.path[0])
+import os
+import neo4j
+
+#paths
+current_dir = sys.path[0] #get current directory from system
+data_dir = current_dir + '/data' #data path
+modules_dir = current_dir + '/modules' #modules path
+
+#add paths to system
+sys.path.append(data_dir) #add data path to system
+sys.path.append(modules_dir) #add modules path to system
+nodes_file = os.path.join(data_dir, 'nodes.tsv') #validate nodes.tsv data file which is in seperate directory
+
+#reading nodes
+from readNodes import createNodes
+createNodes(nodes_file)
--- modules/readNodes.py
+++ modules/readNodes.py
@@ -1,6 +1,8 @@
-print("Loading readNodes module......")
+import csv
 
 def createNodes(file_path):
     with open(file_path) as nodes:
-        for line in nodes:
-            print(line)
+        data = csv.reader(nodes, delimiter='\t')
+        rows = list(data)
+        for i in range(100):
+            print(i, rows[i])
